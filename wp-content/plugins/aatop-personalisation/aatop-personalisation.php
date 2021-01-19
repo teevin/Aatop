716246dd5954cbb6937de240562daf49ce538133
Tinashe <ttsvigu@sqli.com>
2021-01-19 19:02:42 +0200

    original requested functionality set

--- wp-content/plugins/aatop-personalisation/aatop-personalisation.php
+++ wp-content/plugins/aatop-personalisation/aatop-personalisation.php
@@ -54,7 +54,124 @@ function aatop_admin_menu()
     require_once plugin_dir_path(__FILE__) . 'classes/adminhtml.php';
     AatopInit::add_aatop_admin_menu();
 }
+function aatop_cookies()
+{
+   // $_GET[‘fwp_zoeken’]
+    $post_tags = [];
+    $search_tags = [];
+    $data = [];
+    $post_type = get_post_type();
+    if (!isset($_COOKIE['id'])) {
+        $data["identified"] = hash('md5', uniqid(time()));
+        setCookie('id', $data["identified"]);
+    } else {
+        $data["identified"] = $_COOKIE['id'];
+    }
+    require_once plugin_dir_path(__FILE__) . 'classes/tracking.php';
+    $tracking  = new AatopTracking($data["identified"]);
+    if (isset($_GET['s'])) {
+        $search_tags[] = $_GET['s'];
+        if(count($search_tags) > 0) {
+            $data["type"]  = "search";
+            $data["tags"]  = $search_tags;
+            $tracking::saveData($data);
+        }
+    }
 
+    if ($post_type == 'vacature') {
+        $post = get_post();
+        $tags = get_the_terms($post, 'aatopvacancy_autocomplete');
+        if (is_array($tags)) {
+            foreach ($tags as $tag) {
+                $post_tags[] = $tag->name;
+            }
+        }
+        if(count($post_tags) > 0 && !isset($_GET['s'])) {
+            $data["type"]  = "vacature";
+            $data["tags"]  = $post_tags;
+            $tracking::saveData($data);
+        }
+    }
+    //2d631a2905d13a19172575a579f7fa23
+    $hits = $tracking::autocomplete_tags($_COOKIE['id']);//$_COOKIE['id']
+    // var_dump($hits);
+}
+
+/**Test functions */
+function register_vacature()
+{
+    register_post_type(
+        'vacature',
+        [
+        'labels'              => [
+        'name'               => 'Vacatures',
+        'singular_name'      => 'Vacature',
+        'all_items'          => 'Alle Vacatures',
+        'add_new'            => 'Nieuwe Vacature',
+        'add_new_item'       => 'Nieuwe Vacature',
+        'edit'               => 'Vacature bewerken',
+        'edit_item'          => 'Vacature bewerken',
+        'new_item'           => 'Nieuwe Vacature',
+        'view'               => 'Vacature bekijken',
+        'view_item'          => 'Vacatures bezoeken',
+        'search_items'       => 'Zoek Vacature',
+        'not_found'          => 'Geen Vacature gevonden',
+        'not_found_in_trash' => 'Geen Vacature gevonden in de prullenbak',
+        ],
+        'public'              => true,
+        'show_in_rest'  => true,
+        'publicly_queryable'  => true,
+        'show_ui'             => true,
+        'capability_type'     => 'post',
+        'hierarchical'        => true,
+        'exclude_from_search' => false,
+        'query_var'           => true,
+        'rewrite'             => true,
+        'enable_search'  => true,
+        'menu_position'       => 41,
+        'menu_icon'           => 'dashicons-format-aside',
+        'supports'            => [ 'title', 'editor' ],
+    
+        ]
+    );
+}
+
+function create_aatopvacancy_autocomplete()
+{
+    $labels = [
+    'name' => _x('Autocompletes', 'taxonomy general name'),
+    'singular_name' => _x('Autocomplete', 'taxonomy singular name'),
+    'search_items' =>  __('Search Autocompletes'),
+    'all_items' => __('All Autocompletes'),
+    'parent_item' => __('Parent Autocomplete'),
+    'parent_item_colon' => __('Parent Autocomplete:'),
+    'edit_item' => __('Edit Autocomplete'),
+    'update_item' => __('Update Autocomplete'),
+    'add_new_item' => __('Add New Autocomplete'),
+    'new_item_name' => __('New Autocomplete Name'),
+    'menu_name' => __('Autocompletes'),
+    ];
+    register_taxonomy(
+        'aatopvacancy_autocomplete',
+        ['vacature'],
+        [
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_in_rest' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'enable_search' => true,
+        'rewrite' => [ 'slug' => 'autocomplete' ],
+        ]
+    );
+}
+add_action('init', 'register_vacature');
+add_action('init', 'create_aatopvacancy_autocomplete');
+/**
+ * End Test
+ */
+add_action('wp_head', 'aatop_cookies', 1);
 register_activation_hook(__FILE__, 'activate_aatop');
 add_action('admin_menu', 'aatop_admin_menu');
 register_deactivation_hook(__FILE__, 'deactivate_aatop');
